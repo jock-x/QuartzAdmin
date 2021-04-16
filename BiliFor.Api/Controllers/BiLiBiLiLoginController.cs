1d32e41d4442e7241faf64708ea53ae9cde94a66
xsw <864015769@qq.com>
2021-04-16 14:45:28 +0800

    新增B站登录验证接口

---
+++ BiliFor.Api/Controllers/BiLiBiLiLoginController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+/// <summary>
+/// 
+/// </summary>
+namespace BiliFor.Api.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class BiLiBiLiLoginController : Controller
+    {
+
+      
+        private readonly ILogger<BiLiBiLiLoginController> _logger;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+      
+        /// <param name="logger"></param>
+        public BiLiBiLiLoginController( ILogger<BiLiBiLiLoginController> logger)
+        {
+        
+            _logger = logger;
+        }
+
+
+    }
+}
