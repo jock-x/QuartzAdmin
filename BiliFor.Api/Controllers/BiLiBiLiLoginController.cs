7777032b2c6a71f6f113b9b4af65ac74ed467dd7
hw-zhuangzhaoming <zhuangzhaoming@hisilicon.com>
2021-04-26 17:09:33 +0800

    修改测试

--- BiliFor.Api/Controllers/BiLiBiLiLoginController.cs
+++ BiliFor.Api/Controllers/BiLiBiLiLoginController.cs
@@ -133,13 +133,16 @@ namespace BiliFor.Api.Controllers
         public async Task<string> GetUserInfo()
         {
             string cookie = "CURRENT_FNVAL=80; _uuid=846FF8F2-C882-C1CD-808F-DEEA4719175585066infoc; blackside_state=1; rpdid=|(m)mJumk)R0J'uYuku|kmm|; buvid3=BB379260-46D0-407B-B31A-086227A133E1184999infoc; LIVE_BUVID=AUTO4416177595741756; fingerprint3=5bdd4f9e03267a2fa34c274964a20a2b; buvid_fp=BB379260-46D0-407B-B31A-086227A133E1184999infoc; bp_article_offset_4830365=510771353527463951; fingerprint_s=22fdbf4a2d532409728945361a3a8c15; bp_t_offset_4830365=514104505842011394; bp_video_offset_4830365=514160147147869440; PVID=1; bfe_id=1e33d9ad1cb29251013800c68af42315; fingerprint=bf201b8cab1033a1ec48ac66ca6093a4; buvid_fp_plain=3536A90E-676D-49F6-8195-3D36856858C4138393infoc; SESSDATA=524a2906,1634354147,94f35*41; bili_jct=884c71e8fcc5798e65461d34baaac988; DedeUserID=4830365; DedeUserID__ckMd5=3d3f94374f6dd343; sid=d3a6p9a7";
+           
+            //获取登录用户信息
             BiliUserInfo apiResponse = _biliaccountservice.LoginByCookie(cookie);
 
+
+            //获取每日任务
             DailyTaskInfo daily = _biliaccountservice.GetDailyTaskStatus(cookie);
 
+            //漫画签到
             _managadomainservice.MangaSign(cookie);
-
-
             return "";
         }
     }
